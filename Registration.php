0b7a8362c06aab11b3c97cc4a5758884e17aa0e9
hemanthj21 <99715332+hemanthj21@users.noreply.github.com>
2023-02-28 22:10:24 -0800

    Add files via upload
    init

---
+++ Registration.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Registration</title>
+  </head>
+  <body>
+      <h1>Registration</h1>
+      username &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; password
+      <form action="/process.php" method="post">
+        <input type="text" name="username">
+        <input type="password" name="password">
+        <input type="submit" value = "Register">
+      </form>
+  </body
+</html>
